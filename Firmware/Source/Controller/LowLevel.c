b5b5de84004f32bc312a2ae3b7b97880416ffed1
IvanovDmtr <d.ivanov@protonelectrotex.com>
2020-10-08 13:05:24 +0300

    Добавлены функции нижнего уровня

--- Firmware/Source/Controller/LowLevel.c
+++ Firmware/Source/Controller/LowLevel.c
@@ -12,50 +12,96 @@ void LL_ToggleBoardLED()
 }
 //-----------------------------
 
-void LL_SetStateExtLed(bool State)
+void LL_SetStateExtPowerLed(bool State)
 {
-	GPIO_SetState(GPIO_EXT_LED, State);
+	GPIO_SetState(GPIO_EXT_LED1, State);
 }
 //-----------------------------
 
-void LL_SetStateFan(bool State)
+void LL_SetStateExtMsrLed(bool State)
 {
-	GPIO_SetState(GPIO_FAN, State);
+	GPIO_SetState(GPIO_EXT_LED2, State);
 }
 //-----------------------------
 
-void LL_SetStateExtLineSync1(bool State)
-{
-	GPIO_SetState(GPIO_EXT_SYNC1_OUT, State);
-}
-//-----------------------------
-
-void LL_SetStateExtLineSync2(bool State)
-{
-	GPIO_SetState(GPIO_EXT_SYNC2_OUT, State);
-}
-//-----------------------------
-
-void LL_SetStateIntLineSync1(bool State)
+void LL_SetStateInterruptLineSync1(bool State)
 {
 	GPIO_SetState(GPIO_INT_SYNC1_OUT, State);
 }
 //-----------------------------
 
-void LL_SetStateIntLineSync2(bool State)
+void LL_SetStateInterruptLineSync2(bool State)
 {
 	GPIO_SetState(GPIO_INT_SYNC2_OUT, State);
 }
 //-----------------------------
 
-bool LL_GetStateIntLineSync1()
+bool LL_GetStateInterruptLineSync1()
 {
 	return GPIO_GetState(GPIO_INT_SYNC1_IN);
 }
 //-----------------------------
 
-bool LL_GetStateIntLineSync2()
+bool LL_GetStateInterruptLineSync2()
 {
 	return GPIO_GetState(GPIO_INT_SYNC2_IN);
 }
 //-----------------------------
+
+bool LL_ArePowerSuppliesReady()
+{
+	return !GPIO_GetState(GPIO_PS_STATES);
+}
+//-----------------------------
+
+void LL_SetCurrentRage(uint16_t Range)
+{
+	switch(Range)
+	{
+		case 0:
+		{
+			GPIO_SetState(GPIO_R0_M, true);
+			GPIO_SetState(GPIO_R1_M, false);
+			GPIO_SetState(GPIO_R2_M, false);
+		}
+		break;
+
+		case 1:
+		{
+			GPIO_SetState(GPIO_R0_M, false);
+			GPIO_SetState(GPIO_R1_M, true);
+			GPIO_SetState(GPIO_R2_M, false);
+		}
+		break;
+
+		case 2:
+		{
+			GPIO_SetState(GPIO_R0_M, false);
+			GPIO_SetState(GPIO_R1_M, false);
+			GPIO_SetState(GPIO_R2_M, true);
+		}
+		break;
+
+		default:
+		{
+			GPIO_SetState(GPIO_R0_M, true);
+			GPIO_SetState(GPIO_R1_M, false);
+			GPIO_SetState(GPIO_R2_M, false);
+		}
+		break;
+	}
+}
+
+void LL_DiscreteOpAmpWriteDAC(uint16_t Data, GPIO_PortPinSetting CS_SYNC)
+{
+	GPIO_SetState(CS_SYNC, false);
+	SPI_WriteByte(SPI1, Data);
+	GPIO_SetState(CS_SYNC, true);
+	DELAY_US(1);
+
+	GPIO_SetState(GPIO_LDAC, false);
+	DELAY_US(5);
+	GPIO_SetState(GPIO_LDAC, true);
+	DELAY_US(1);
+}
+//---------------------
--- Firmware/Source/Controller/LowLevel.h
+++ Firmware/Source/Controller/LowLevel.h
@@ -7,14 +7,14 @@
 // Functions
 //
 void LL_ToggleBoardLED();
-void LL_SetStateExtLed(bool State);
-void LL_SetStateFan(bool State);
-void LL_SetStateExtLineSync1(bool State);
-void LL_SetStateExtLineSync2(bool State);
-void LL_SetStateIntLineSync1(bool State);
-void LL_SetStateIntLineSync2(bool State);
-
-bool LL_GetStateIntLineSync1();
-bool LL_GetStateIntLineSync2();
+void LL_SetStateExtPowerLed(bool State);
+void LL_SetStateExtMsrLed(bool State);
+void LL_SetStateInterruptLineSync1(bool State);
+void LL_SetStateInterruptLineSync2(bool State);
+bool LL_GetStateInterruptLineSync1();
+bool LL_GetStateInterruptLineSync2();
+bool LL_ArePowerSuppliesReady();
+void LL_SetCurrentRage(uint16_t Range);
+void LL_DiscreteOpAmpWriteDAC(uint16_t Data, GPIO_PortPinSetting CS_SYNC);
 
 #endif //__LOWLEVEL_H
